e81f2b9c012f28d76c611d9392199f32b778ee8c
LiJun <lij@terminus.io>
2020-03-02 21:26:58 +0800

    feat: add redux dev tools plugin

---
+++ src/plugin/dev-tool.js
@@ -0,0 +1,29 @@
+export default ({
+  use,
+  storeMap,
+}) => {
+  let extension;
+  try {
+    extension =
+      window.__REDUX_DEVTOOLS_EXTENSION__ ||
+      window.top.__REDUX_DEVTOOLS_EXTENSION__;
+  } catch { }
+
+  if (!extension) {
+    if (process.env.NODE_ENV === 'development') {
+      console.warn('Please install/enable Redux devtools extension');
+    }
+    return;
+  }
+  const newStore = {};
+  const devtools = extension.connect();
+  use({
+    afterReducer({ storeName, reducerName, payload }) {
+      Object.keys(storeMap).forEach((k) => {
+        newStore[k] = storeMap[k].getState();
+      });
+      devtools.send({ type: `[${storeName}] > ${reducerName}`, payload }, newStore);
+    },
+  });
+  devtools.init(storeMap);
+};
