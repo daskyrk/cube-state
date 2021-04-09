8209cb411b1f985b9ab70153714cad7c07ebfb9a
老俊 <782803291@qq.com>
2021-04-09 17:30:08 +0800

    feat: add allow duplicated store

--- src/typings.ts
+++ src/typings.ts
@@ -19,6 +19,7 @@ export declare namespace CubeState {
     extendEffect?(config: extendEffectConfig): object;
     onCreate?(store: StoreItem): any;
     pureChecker?(fnName: string): boolean;
+    allowDuplicatedStore?: boolean;
   }
 
   interface Opt<S, R, E> {
